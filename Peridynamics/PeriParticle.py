ab6cd580298eb9ad761c0fe54bbbface23112a14
Jim Madge <jmmadge@gmail.com>
2020-01-13 09:48:50 +0000

    Tidy PeriParticle

--- Peridynamics/PeriParticle.py
+++ Peridynamics/PeriParticle.py
@@ -1,48 +1,47 @@
 class Particle:
+    def __init__(self):
+        self.family = []
+        self.V = 0.0
+        self.U = []
+        self.tsteps = 1
+        self.damage = []
+        self.damage.append(0)
 
-	def __init__(self):
-		self.family = []
-		self.V = 0.0;
-		self.U = []
-		self.tsteps = 1
-		self.damage = []
-		self.damage.append(0)
+    def setDisp(self, U):
+        self.U.append(U)
 
-	def setDisp(self, U_):
-		self.U.append(U_)
+    def getDisp(self, time):
+        return self.U[time]
 
-	def getDisp(self, time):
-		return self.U[time]
+    def setDamage(self, val, time):
+        if (time > self.tsteps):
+            self.damage.append(val)
+        else:
+            self.damage[time] = val
 
-	def setDamage(self, val_, time):
-		if (time > self.tsteps):
-			self.damage.append(val_)
-		else :
-			self.damage[time] = val_
+    def getDamage(self, time):
+        return self.damage[time]
 
-	def getDamage(self,time):
-		return self.damage[time]
+    def setId(self, _id):
+        self.id = _id
 
-	def setId(self, id_):
-		self.id = id_
+    def setCoord(self, x):
+        self.x = x
 
-	def setCoord(self, x_):
-		self.x = x_
+    def addNeighbour(self, j):
+        self.family.append(j)
 
-	def addNeighbour(self, j):
-		(self.family).append(j)
+    def getFamily(self):
+        return self.family
 
-	def getFamily(self):
-		return self.family
+    def getX(self):
+        return self.x
 
-	def getX(self):
-		return self.x
+    def addVolume(self, vol):
+        self.V += vol
 
-	def addVolume(self, vol_):
-		self.V += vol_
+    def getVolume(self):
+        return self.V
 
-	def getVolume(self):
-		return self.V
-
-	def getFamilySize(self):
-		return len(self.family)
+    def getFamilySize(self):
+        return len(self.family)
