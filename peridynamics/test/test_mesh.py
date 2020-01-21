cc7b812fae1aefd052a689a0a442a047b772700c
Jim Madge <jmmadge@gmail.com>
2020-01-21 11:34:42 +0000

    Add read_mesh tests

---
+++ peridynamics/test/test_mesh.py
@@ -0,0 +1,42 @@
+"""
+Tests for the reading and writing of mesh data
+"""
+from ..SeqPeriVectorized import SeqModel as Model
+import numpy as np
+import pathlib
+import pytest
+
+
+@pytest.fixture
+def basic_model():
+    mesh_file = pathlib.Path(__file__).parent.absolute() / "regression.msh"
+    model = Model()
+    model.read_mesh(mesh_file)
+
+    return model
+
+
+class TestRead:
+    def test_coords(self, basic_model):
+        model = basic_model
+
+        assert model.coords.shape == (2113, 3)
+        assert model.nnodes == 2113
+        assert np.all(
+            model.coords[42] == np.array([1., 0.2499999999994083, 0.]))
+
+    def test_connectivity(self, basic_model):
+        model = basic_model
+
+        assert model.connectivity.shape == (4096, 3)
+        assert model.nelem == 4096
+        assert np.all(
+            model.connectivity[100] == np.array([252, 651, 650]))
+
+    def test_boundary_connectivity(self, basic_model):
+        model = basic_model
+
+        assert model.connectivity_bnd.shape == (128, 2)
+        assert model.nelem_bnd == 128
+        assert np.all(
+            model.connectivity_bnd[100] == np.array([100, 101]))
