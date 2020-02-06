01c5a7832eeb32c03df225aff6a650a51d60502b
Jim Madge <jmmadge@gmail.com>
2020-02-06 14:29:20 +0000

    Set autodoc to document __init__ methods

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
+autoclass_content = 'both'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
