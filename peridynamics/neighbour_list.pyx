0741d3a7ff9ef7bc3c6d4e4e4d3a3431b8b7eaa3
Jim Madge <jmmadge@gmail.com>
2020-04-09 10:39:10 +0100

    Add bond force function

--- peridynamics/neighbour_list.pyx
+++ peridynamics/neighbour_list.pyx
@@ -114,3 +114,42 @@ def damage(int[:] n_neigh, int[:] family):
         result_view[i] = (ifamily - n_neigh[i])/ifamily
 
     return result
+
+
+def bond_force(double[:, :] r, double[:, :] r0, int[:, :] nlist,
+               int[:] n_neigh, double[:] volume, double bond_stiffness):
+    cdef int nnodes = nlist.shape[0]
+
+    force = np.zeros((nnodes, 3), dtype=np.float64)
+    cdef double[:, :] force_view = force
+
+    cdef int i, j, dim, i_n_neigh, neigh
+    cdef double strain, l, force_norm
+    cdef double[3] f
+
+    for i in range(nnodes):
+        i_n_neigh = n_neigh[i]
+        for neigh in range(i_n_neigh):
+            j = nlist[i, neigh]
+
+            if i < j:
+                strain = cstrain(r[i], r[j], r0[i], r0[j])
+                l = ceuclid(r[i], r[j])
+
+                force_norm = strain * bond_stiffness
+                # Calculate component of force in each dimension
+                force_norm = force_norm / l
+                for dim in range(3):
+                    f[dim] = force_norm * (r[j, dim] - r[i, dim])
+
+                # Add force to particle i, using Newton's third law subtract
+                # force from j
+                for dim in range(3):
+                    force_view[i, dim] = force_view[i, dim] + f[dim]
+                    force_view[j, dim] = force_view[j, dim] - f[dim]
+
+        # Scale force by node volume
+        for dim in range(3):
+            force_view[i, dim] = force_view[i, dim] * volume[i]
+
+    return force
--- peridynamics/test/test_neighbour_list.py
+++ peridynamics/test/test_neighbour_list.py
@@ -1,6 +1,7 @@
 """Tests for the neighbour list module."""
 from peridynamics.neighbour_list import (
-    euclid, strain, family, create_neighbour_list, break_bonds, damage
+    euclid, strain, family, create_neighbour_list, break_bonds, damage,
+    bond_force
     )
 import numpy as np
 from scipy.spatial.distance import euclidean, cdist
@@ -160,3 +161,58 @@ def test_damage():
     damage_expected = (family - n_neigh) / family
 
     assert np.allclose(damage_actual, damage_expected)
+
+
+class TestForce():
+    """Test force calculation."""
+
+    def test_initial_force(self):
+        """Ensure forces are zero when there is no displacement."""
+        r0 = np.array([
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [2.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0],
+            ])
+        horizon = 1.1
+        volume = np.ones(5)
+        bond_stiffness = 1.0
+        nl, n_neigh = create_neighbour_list(r0, horizon, 3)
+
+        force_expected = np.zeros((5, 3))
+        force_actual = bond_force(r0, r0, nl, n_neigh, volume, bond_stiffness)
+
+        assert np.allclose(force_actual, force_expected)
+
+    def test_force(self):
+        """Ensure forces are in the correct direction using a minimal model."""
+        r0 = np.array([
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [1.0, 1.0, 0.0],
+            ])
+        horizon = 1.01
+        elastic_modulus = 0.05
+        bond_stiffness = 18.0 * elastic_modulus / (np.pi * horizon**4)
+        volume = np.full(3, 0.16666667)
+        nl, n_neigh = create_neighbour_list(r0, horizon, 3)
+
+        # Displace particles, but do not update neighbour list
+        r = r0 + np.array([
+            [0.0, 0.0, 0.0],
+            [0.05, 0.0, 0.0],
+            [0.05, 0.05, 0.0]
+            ])
+
+        actual_force = bond_force(r, r0, nl, n_neigh, volume,
+                                  bond_stiffness)
+
+        # Ensure force array is correct
+        force_value = 0.00229417
+        expected_force = np.array([
+            [force_value, 0., 0.],
+            [-force_value, force_value, 0.],
+            [0., -force_value, 0.]
+            ])
+        assert np.allclose(actual_force, expected_force)
