d895f7c129f6857cf287e7a06512a3f8e77c81e0
Jim Madge <jmmadge@gmail.com>
2020-04-29 15:33:34 +0100

    Print context info

---
+++ context_info.py
@@ -0,0 +1,16 @@
+import pyopencl as cl
+
+DOUBLE_FP_SUPPORT = (
+    cl.device_fp_config.DENORM | cl.device_fp_config.FMA |
+    cl.device_fp_config.INF_NAN | cl.device_fp_config.ROUND_TO_INF |
+    cl.device_fp_config.ROUND_TO_NEAREST |
+    cl.device_fp_config.ROUND_TO_ZERO
+    )
+
+for platform in cl.get_platforms():
+    for device_type in [cl.device_type.GPU, cl.device_type.ALL]:
+        print(cl.device_type.to_string(device_type))
+        for device in platform.get_devices(device_type):
+            print(f"\t{device}")
+            print(f"\t{device.get_info(cl.device_info.DOUBLE_FP_CONFIG)}")
+            print(f"\t{device.get_info(cl.device_info.DOUBLE_FP_CONFIG) == DOUBLE_FP_SUPPORT}")
