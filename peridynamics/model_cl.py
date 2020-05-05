375dca586f3e74dff1743663abb6b14ea4203828
Jim Madge <jmmadge@gmail.com>
2020-05-05 10:11:56 +0100

    Inline

--- peridynamics/model_cl.py
+++ peridynamics/model_cl.py
@@ -1,9 +1,12 @@
 """Peridynamics model using OpenCL kernels."""
-from .model import Model
+from .integrators import Integrator
+from .model import Model, InvalidIntegrator
 from .cl import double_fp_support, get_context, kernel_source
 import numpy as np
+import pathlib
 import pyopencl as cl
 from pyopencl import mem_flags as mf
+from tqdm import trange
 
 
 class ModelCL(Model):
@@ -139,6 +142,160 @@ class ModelCL(Model):
         queue.finish()
         cl.enqueue_copy(queue, force, force_d)
         return force
+
+    def simulate(self, steps, integrator, boundary_function=None, u=None,
+                 connectivity=None, first_step=1, write=None, write_path=None):
+        """
+        Simulate the peridynamics model.
+
+        :arg int steps: The number of simulation steps to conduct.
+        :arg  integrator: The integrator to use, see
+            :mod:`peridynamics.integrators` for options.
+        :type integrator: :class:`peridynamics.integrators.Integrator`
+        :arg boundary_function: A function to apply the boundary conditions for
+            the simlation. It has the form
+            boundary_function(:class:`peridynamics.model.Model`,
+            :class:`numpy.ndarray`, `int`). The arguments are the model being
+            simulated, the current displacements, and the current step number
+            (beginning from 1). `boundary_function` returns a (nnodes, 3)
+            :class:`numpy.ndarray` of the updated displacements
+            after applying the boundary conditions. Default `None`.
+        :type boundary_function: function
+        :arg u: The initial displacements for the simulation. If `None` the
+            displacements will be initialised to zero. Default `None`.
+        :type u: :class:`numpy.ndarray`
+        :arg connectivity: The initial connectivity for the simulation. A tuple
+            of a neighbour list and the number of neighbours for each node. If
+            `None` the connectivity at the time of construction of the
+            :class:`Model` object will be used. Default `None`.
+        :type connectivity: tuple(:class:`numpy.ndarray`,
+            :class:`numpy.ndarray`)
+        :arg int first_step: The starting step number. This is useful when
+            restarting a simulation, especially if `boundary_function` depends
+            on the absolute step number.
+        :arg int write: The frequency, in number of steps, to write the system
+            to a mesh file by calling :meth:`Model.write_mesh`. If `None` then
+            no output is written. Default `None`.
+        :arg write_path: The path where the periodic mesh files should be
+            written.
+        :type write_path: path-like or str
+
+        :returns: A tuple of the final displacements (`u`), damage and
+            connectivity.
+        :rtype: tuple(:class:`numpy.ndarray`, :class:`numpy.ndarray`,
+            tuple(:class:`numpy.ndarray`, :class:`numpy.ndarray`))
+        """
+        if not isinstance(integrator, Integrator):
+            raise InvalidIntegrator(integrator)
+
+        # Create initial displacements is none is provided
+        if u is None:
+            u = np.zeros((self.nnodes, 3))
+
+        # Use the initial connectivity (when the Model was constructed) if none
+        # is provided
+        if connectivity is None:
+            nlist, n_neigh = self.initial_connectivity
+        elif type(connectivity) == tuple:
+            if len(connectivity) != 2:
+                raise ValueError("connectivity must be of size 2")
+            nlist, n_neigh = connectivity
+        else:
+            raise TypeError("connectivity must be a tuple or None")
+
+        # Create dummy boundary conditions function is none is provided
+        if boundary_function is None:
+            def boundary_function(model, u, step):
+                return u
+
+        # If no write path was provided use the current directory, otherwise
+        # ensure write_path is a Path object.
+        if write_path is None:
+            write_path = pathlib.Path()
+        else:
+            write_path = pathlib.Path(write_path)
+
+        context = self.context
+        queue = self.queue
+
+        for step in trange(first_step, first_step+steps,
+                           desc="Simulation Progress", unit="steps"):
+
+            # Calculate the force due to bonds on each node
+            # f = self._bond_force(u, nlist, n_neigh)
+            force = np.empty_like(self.coords)
+
+            # Create buffers
+            r_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                            hostbuf=self.coords+u)
+            r0_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                             hostbuf=self.coords)
+            nlist_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                                hostbuf=nlist)
+            n_neigh_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                                  hostbuf=n_neigh)
+            volume_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                                 hostbuf=self.volume)
+            force_d = cl.Buffer(context, mf.WRITE_ONLY, force.nbytes)
+
+            # Call kernel
+            self.bond_force_kernel(queue, n_neigh.shape, None, r_d, r0_d,
+                                   nlist_d,
+                                   n_neigh_d, np.int32(self.max_neighbours),
+                                   volume_d, np.float64(self.bond_stiffness),
+                                   force_d)
+            queue.finish()
+            cl.enqueue_copy(queue, force, force_d)
+            f = force
+
+            # Conduct one integration step
+            u = integrator(u, f)
+            # Apply boundary conditions
+            u = boundary_function(self, u, step)
+
+            # Update neighbour list
+            # self._break_bonds(u, nlist, n_neigh)
+            # Create buffers
+            r_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                            hostbuf=self.coords+u)
+            r0_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                             hostbuf=self.coords)
+            nlist_d = cl.Buffer(context, mf.READ_WRITE | mf.COPY_HOST_PTR,
+                                hostbuf=nlist)
+            n_neigh_d = cl.Buffer(context, mf.READ_WRITE | mf.COPY_HOST_PTR,
+                                  hostbuf=n_neigh)
+
+            # Call kernel
+            self.break_bonds_kernel(queue, n_neigh.shape, None, r_d, r0_d,
+                                    nlist_d,
+                                    n_neigh_d, np.int32(self.max_neighbours),
+                                    np.float64(self.critical_strain))
+            queue.finish()
+            cl.enqueue_copy(queue, nlist, nlist_d)
+            cl.enqueue_copy(queue, n_neigh, n_neigh_d)
+
+            # Calculate the current damage
+            # damage = self._damage(n_neigh)
+            damage = np.empty(n_neigh.shape, dtype=np.float64)
+
+            # Create buffers
+            n_neigh_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                                  hostbuf=n_neigh)
+            family_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR,
+                                 hostbuf=self.family)
+            damage_d = cl.Buffer(context, mf.WRITE_ONLY, damage.nbytes)
+
+            # Call kernel
+            self.damage_kernel(queue, damage.shape, None, n_neigh_d, family_d,
+                               damage_d)
+            queue.finish()
+            cl.enqueue_copy(queue, damage, damage_d)
+
+            if write:
+                if step % write == 0:
+                    self.write_mesh(write_path/f"U_{step}.vtk", damage, u)
+
+        return u, damage, (nlist, n_neigh)
 
 
 class ContextError(Exception):
